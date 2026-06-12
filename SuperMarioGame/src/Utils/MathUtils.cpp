acbf6a46607f8a1821fbe3a039061f1b0dfff194
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-06-12 19:10:07 +0700

    feat: complete MathUtils vector operations

--- SuperMarioGame/src/Utils/MathUtils.cpp
+++ SuperMarioGame/src/Utils/MathUtils.cpp
@@ -1,18 +1,20 @@
 #include "Utils/MathUtils.hpp"
+#include <cmath>
 
 namespace MathUtils {
     float magnitude(const sf::Vector2f& vector) {
-        // TODO: Implement by hand
-        return 0.0f;
+        return std::sqrt(vector.x * vector.x + vector.y * vector.y);
     }
 
     sf::Vector2f normalize(const sf::Vector2f& vector) {
-        // TODO: Implement by hand
+        float mag = magnitude(vector);
+        if (mag > 0.0f) {
+            return vector / mag;
+        }
         return sf::Vector2f{0.0f, 0.0f};
     }
 
     float distance(const sf::Vector2f& p1, const sf::Vector2f& p2) {
-        // TODO: Implement by hand
-        return 0.0f;
+        return magnitude(p2 - p1);
     }
 }
