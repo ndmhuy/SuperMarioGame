9be1d1a5d9b1bf230fad353f0fa72d15dbaab38a
Nguyen Dinh Minh Huy <ndmhuy2521@apcs.fitus.edu.vn>
2026-06-16 20:53:17 +0700

    docs: add strict OOP encapsulation getter/setter rules to AGENTS.md

--- SuperMarioGame/include/Core/EventBus.hpp
+++ SuperMarioGame/include/Core/EventBus.hpp
@@ -6,59 +6,59 @@
 #include <vector>
 
 enum class EventType {
-    CoinCollected,
-    EnemyDefeated,
-    PlayerDied,
-    PowerUpCollected,
-    LevelComplete,
-    ComboHit,
-    AchievementUnlocked,
-    StarCoinCollected,
-    PSwitchActivated,
-    BossDefeated,
-    CheckpointActivated,
-    PlayerDamaged,
-    BlockBroken,
-    TimeWarning,
-    GameOver,
-    GameStart,
-    StateChanged,
-    PauseToggled
+  CoinCollected,
+  EnemyDefeated,
+  PlayerDied,
+  PowerUpCollected,
+  LevelComplete,
+  ComboHit,
+  AchievementUnlocked,
+  StarCoinCollected,
+  PSwitchActivated,
+  BossDefeated,
+  CheckpointActivated,
+  PlayerDamaged,
+  BlockBroken,
+  TimeWarning,
+  GameOver,
+  GameStart,
+  StateChanged,
+  PauseToggled
 };
 
 struct GameEvent {
-    EventType type;
-    std::any data;
+  EventType type;
+  std::any data;
 };
 
 class EventBus {
 public:
-    using Callback = std::function<void(const GameEvent&)>;
-    using SubscriptionId = size_t;
+  using Callback = std::function<void(const GameEvent &)>;
+  using SubscriptionId = size_t;
 
-    // Delete copy/move semantics for Singleton
-    EventBus(const EventBus&) = delete;
-    EventBus& operator=(const EventBus&) = delete;
-    EventBus(EventBus&&) = delete;
-    EventBus& operator=(EventBus&&) = delete;
+  // Delete copy/move semantics for Singleton
+  EventBus(const EventBus &) = delete;
+  EventBus &operator=(const EventBus &) = delete;
+  EventBus(EventBus &&) = delete;
+  EventBus &operator=(EventBus &&) = delete;
 
-    // Singleton Instance
-    static EventBus& getInstance();
+  // Singleton Instance
+  static EventBus &getInstance();
 
-    // Event operations
-    SubscriptionId subscribe(EventType type, Callback callback);
-    void unsubscribe(SubscriptionId id);
-    void publish(const GameEvent& event);
+  // Event operations
+  SubscriptionId subscribe(EventType type, Callback callback);
+  void unsubscribe(SubscriptionId id);
+  void publish(const GameEvent &event);
 
 private:
-    EventBus() = default;
-    ~EventBus() = default;
+  EventBus() = default;
+  ~EventBus() = default;
 
-    struct Subscription {
-        SubscriptionId id;
-        Callback callback;
-    };
+  struct Subscription {
+    SubscriptionId id;
+    Callback callback;
+  };
 
-    std::unordered_map<EventType, std::vector<Subscription>> m_subscribers;
-    SubscriptionId m_nextId = 0;
+  std::unordered_map<EventType, std::vector<Subscription>> m_subscribers;
+  SubscriptionId m_nextId = 0;
 };
